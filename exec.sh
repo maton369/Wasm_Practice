91554fe160832f97c6da0662cfaba0b781e60f2a
maton369 <ysk901085@gmail.com>
2025-06-23 12:00:02 +0900

    Add: simple.watからsimple.wasmを作成

--- exec.sh
+++ exec.sh
@@ -5,4 +5,5 @@
 # EOF
 # wat2wasm -o minimal.wasm minimal.wat
 # xxd minimal.wasm | tee output.log
-wasm2wat minimal.wasm | tee output.log
+# wasm2wat minimal.wasm | tee output.log
+wat2wasm -o simple.wasm simple.wat
