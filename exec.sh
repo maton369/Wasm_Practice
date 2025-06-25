a57066fb0474c3d57cdcdc50f90f33ab7aa24e0f
maton369 <ysk901085@gmail.com>
2025-06-25 09:22:57 +0900

    Add: Wasmをブラウザ上で実行

--- exec.sh
+++ exec.sh
@@ -6,4 +6,5 @@
 # wat2wasm -o minimal.wasm minimal.wat
 # xxd minimal.wasm | tee output.log
 # wasm2wat minimal.wasm | tee output.log
-wat2wasm -o simple.wasm simple.wat
+# wat2wasm -o simple.wasm simple.wat
+python3 -m http.server | tee output.log
